abca897273cb738d16d16534cd00d22aac08a5f9
aayush <aayushsuthar2004@gmail.com>
2026-04-18 11:15:06 +0530

    note: Will add Token Pool and CCIP in future

---
+++ src/RebaseTokenPool.sol
@@ -0,0 +1,11 @@
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.18;
+
+// import {TokenPool} from "@ccip/contracts/pools/TokenPool.sol";
+// import {Pool} from "ccip/contracts/src/v0.8/ccip/libraries/Pool.sol";
+
+// contract RebaseTokenPool is TokenPool{
+
+//     constructor(IERC20 _token, address[] memory _allowlist, address _rnmProxy, address _router) TokenPool(_token, 18, _allowlist, _rmnProxy, _router) {}
+
+// }
